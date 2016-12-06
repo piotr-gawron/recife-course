3130861f0c4ea41c6f17d803b49a7993131d24ab
Piotr Gawron <piotr.gawron@uni.lu>
2016-12-07 00:10:54 +0100

    task 4 added

---
+++ task-4/src/index.js
@@ -0,0 +1,25 @@
+var request = require("request");
+
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Recife&appid=d531cb6301fd0e227a1c9463f371d0dc"
+
+function showData(data){
+	console.log(getWeatherString(data));
+}
+
+function getWeatherString(data) {
+	var kelvinTemp = data.main.temp;
+	var celsiusTemp = kelvinTemp - 272.15;
+	var place = data.name;
+	var message = "Weaher in "+place+": Temperature: "+celsiusTemp.toFixed(2)+" celsius";
+	return message;
+}
+
+request.get(url, function(error, response, body) {
+	if (error || response.statusCode !== 200) {
+		console.log("Problem with accessing data: ", error, response.statusCode);
+	} else {
+		showData(JSON.parse(body));
+	}
+});
+
+
